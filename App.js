e7eaec8c2e70ba757d8d5100f11c9d583913ffd1
Denis <denis.tojaga@edu.fit.ba>
2021-03-12 18:19:34 +0100

    //creating stack navigator

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./src/screens/indexScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
+
+//createStackNavigator takes two objects
+//1. pairs for indexing each screen
+//2.object for initial show screen
+
+const navigator = createStackNavigator(
+
+  {
+    Index: IndexScreen
+
   },
-});
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Index"
+    }
+  }
+
+);
+
+
+
+
+
+
+export default createAppContainer(navigator);
