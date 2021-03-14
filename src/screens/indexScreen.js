bf67b5e1ea936d269bd5867015f2a09b58bc93f4
Denis <denis.tojaga@edu.fit.ba>
2021-03-14 17:01:16 +0100

    //finished state management

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 
@@ -7,28 +7,33 @@ const IndexScreen = () => {
 
 
     //here we store the value of the context object
-    const blogPosts = useContext(BlogContext);
+    //this gives us  an object with props of data and addBlogPost function
+    const blogProviderObject = useContext(BlogContext);
 
 
 
 
+    //we are passing a reference to a function when cerain button is pressed
 
     return (
         <View>
             <Text>Index Screen!</Text>
+
+            <Button title="Add blogpost" onPress={blogProviderObject.addBlogPost} />
+
             <FlatList
-                data={blogPosts}
+                data={blogProviderObject.data}
                 keyExtractor={(blogPost) => blogPost.title}
-                renderItem = { ({ item }) => {
+                renderItem={({ item }) => {
 
-                return (
-                    <Text>{item.title}</Text>
-                );
+                    return (
+                        <Text>{item.title}</Text>
+                    );
 
-            }}
+                }}
             />
 
-            
+
         </View>
 
 
