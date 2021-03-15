13170fe86561d1fca0200026d1ee695319bfad97
Denis <denis.tojaga@edu.fit.ba>
2021-03-15 15:34:55 +0100

    //bugFix

--- App.js
+++ App.js
@@ -1,25 +1,17 @@
-//everytime we want to use some JSX in our file we need to import React
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import IndexScreen from "./src/screens/indexScreen";
-//because we didn't use the export default in out BlogProvider we must use { }
+import IndexScreen from "./src/screens/IndexScreen";
 import { BlogProvider } from "./src/context/BlogContext";
 
 
-
-
-
-//createStackNavigator takes two objects
-//1. pairs for indexing each screen
-//2.object for initial show screen
 
 const navigator = createStackNavigator(
 
   {
     Index: IndexScreen
-
   },
+
   {
     initialRouteName: "Index",
     defaultNavigationOptions: {
@@ -32,19 +24,20 @@ const navigator = createStackNavigator(
 
 
 
-//when we export createAppContainer(navigator) the react-native export like some sort of component
-//we can assign the whole component to a variable and export a function 
-//wrapping it inside our own custom component
+
+
 const App = createAppContainer(navigator);
+
 
 
 export default () => {
 
-  //reason why we added children props in BlogProvider, because we want it to display some other custom component
   return (
+
     <BlogProvider>
       <App />
     </BlogProvider>
+  
   );
-};
 
+};
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,18 @@
 import React from "react";
-//React's library has a function createContext that helps us to create this new object
-//that is going to handle all CRUD operations 
-
-import createDataContext from "./createDataContext";
 
 
-const reducerFunction = (state, action) => {
+const BlogContext = React.createContext();
 
-    switch (action.type) {
-        case "add_blogpost":
-            return [...state, { title: `Blog post #${state.length + 1}` }];
-        default:
-            return state;
-    }
+
+
+
+export const BlogProvider = ({ children }) => {
+
+    return (
+        <BlogContext.Provider>
+            {children}
+        </BlogContext.Provider>
+    );
 
 };
 
@@ -20,20 +20,6 @@ const reducerFunction = (state, action) => {
 
 
 
-const addBlogPost = (runReducer) => {
-    return () => {
-        runReducer({ type: "add_blogpost" });
-    };
-};
 
-
-
-
-//we export what the function is returning
-//3 arguments we need to pass to that function
-//first one is reducerFunction(where logic happens)
-//second one is the callBack function which will do operation
-//third one is the initial state value
-export const { Context, Provider } = createDataContext(reducerFunction, { addBlogPost }, []);
 
 
