184e83a19ded1cf7931d69f2b1611b40a0535b94
Denis <denis.tojaga@edu.fit.ba>
2021-03-15 16:05:42 +0100

    //update

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 
 const BlogContext = React.createContext();
@@ -8,13 +9,35 @@ const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
 
+    const [blogPosts, setBlogPosts] = useState([]);
+
+
+
+
+    //callback function
+    const addBlogPost = () => {
+
+        //copy everything from the old array and place it in new one
+        //after that add the new object with title prop
+        setBlogPosts([...blogPosts, { title: `Blog post${blogPosts.length + 1}` }]);
+
+
+    };
+
+
+
+
     return (
-        <BlogContext.Provider>
+        <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
             {children}
         </BlogContext.Provider>
     );
 
 };
+
+
+
+export default BlogContext;
 
 
 
--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import BlogContext from "../context/BlogContext";
+
+
+
+
+
 
 const IndexScreen = () => {
 
-    return <View>
-        <Text>Index screen!</Text>
-    </View>
+
+    //destructuring the props we get from BlogContext component
+    const { data, addBlogPost } = useContext(BlogContext);
+
+
+    return (
+        <View>
+            <Text>Index screen!</Text>
+            <Button title="Add post" onPress={addBlogPost} />
+
+            <FlatList
+                data={data}
+
+                keyExtractor={blogPost => blogPost.title}
+
+                renderItem={({ item }) => {
+
+                    return <Text>{item.title}</Text>
+
+                }}
+            />
+
+
+
+        </View>
+    );
 };
 
 
