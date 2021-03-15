c7c467dc0ee4c1a07c2e01cf84fd5183f6baf469
Denis <denis.tojaga@edu.fit.ba>
2021-03-15 16:22:06 +0100

    //changes

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,28 +1,55 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 
 
+//creation of context object from React library
 const BlogContext = React.createContext();
+
+
+
+
+const blogReducer = (state, action) => {
+
+    switch (action.type) {
+        case "add_blogpost":
+            return [...state, { title: `Blog Post #${state.length + 1}` }];
+
+        default:
+            return state;
+    }
+
+};
 
 
 
 
 export const BlogProvider = ({ children }) => {
 
-    const [blogPosts, setBlogPosts] = useState([]);
+
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
 
 
 
     //callback function
+    // const addBlogPost = () => {
+
+    //     //copy everything from the old array and place it in new one
+    //     //after that add the new object with title prop
+    //     setBlogPosts([...blogPosts, { title: `Blog post${blogPosts.length + 1}` }]);
+    // };
+
+
+
+
+    //after this function gets called we call dispatch that manages adding a new blogpost
     const addBlogPost = () => {
-
-        //copy everything from the old array and place it in new one
-        //after that add the new object with title prop
-        setBlogPosts([...blogPosts, { title: `Blog post${blogPosts.length + 1}` }]);
-
-
+        dispatch({ type: "add_blogpost" });
     };
+
+
+
+
 
 
 
