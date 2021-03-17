0d7f8dc8c27afb590d912f774b5c7d0e3a5b7180
Denis <denis.tojaga@edu.fit.ba>
2021-03-17 11:13:58 +0100

    //create screen added

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
 import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
 import { Provider } from "./src/context/BlogContext";
 
 
@@ -11,7 +12,8 @@ const navigator = createStackNavigator(
 
   {
     Index: IndexScreen,
-    Show:ShowScreen
+    Show:ShowScreen,
+    Create:CreateScreen
   },
 
   {
---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Context } from "../context/BlogContext";
+
+
+
+const CreateScreen = ({ navigation }) => {
+    
+    return (
+        <View>
+            <Text>Create Screen</Text>
+        </View>
+    );
+
+};
+
+
+
+const styles = StyleSheet.create({
+
+
+});
+
+
+
+export default CreateScreen;
