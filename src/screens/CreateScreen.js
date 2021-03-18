d01ae7d30b453339212539abba331bf4907207c3
Denis <denis.tojaga@edu.fit.ba>
2021-03-18 17:13:24 +0100

    //styling for form

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Text, TextInput, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, TextInput, View, StyleSheet, TouchableOpacity,Button } from "react-native";
 import { Context } from "../context/BlogContext";
 
 
@@ -18,11 +18,14 @@ const CreateScreen = ({ navigation }) => {
 
 
     return (
-        <View>
-            <Text>Enter Title:</Text>
-            <TextInput value={title} onChangeText={(text) => setTitle(text)} />
-            <Text>Enter Content:</Text>
-            <TextInput value={content} onChangeText={(content) => setContent(content)} />
+        <View style = {styles.container}>
+
+            <Text style={styles.label} >Enter Title:</Text>
+            <TextInput style={styles.inputStyle} value={title} onChangeText={(text) => setTitle(text)} />
+
+            <Text style={styles.label} >Enter Content:</Text>
+            <TextInput style={styles.inputStyle} value={content} onChangeText={(content) => setContent(content)} />
+
         </View>
     );
 
@@ -32,6 +35,26 @@ const CreateScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
 
+    inputStyle: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: "black",
+        marginBottom: 15,
+        padding: 5,
+        margin: 10,
+        borderRadius:8
+    },
+
+
+    label: {
+        fontSize: 25,
+        fontWeight:"bold",
+        marginHorizontal: 10
+    },
+
+    container:{
+        marginVertical:25
+    }
 
 });
 
