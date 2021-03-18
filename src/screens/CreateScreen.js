e4e8d3da0ccf0b5574d306b6bb74b1cbe599342a
Denis <denis.tojaga@edu.fit.ba>
2021-03-18 17:06:06 +0100

    //initializing states

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,17 +1,28 @@
-import React, { useContext } from "react";
-import { Text,TextInput, View, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useContext, useState } from "react";
+import { Text, TextInput, View, StyleSheet, TouchableOpacity } from "react-native";
 import { Context } from "../context/BlogContext";
 
 
 
 const CreateScreen = ({ navigation }) => {
 
+    //we initialize two pieces of state
+
+    //one to handle title entry 
+    const [title, setTitle] = useState("");
+
+
+    //one to handle the content entry
+    const [content, setContent] = useState("");
+
+
+
     return (
         <View>
             <Text>Enter Title:</Text>
-            <TextInput />
+            <TextInput value={title} onChangeText={(text) => setTitle(text)} />
             <Text>Enter Content:</Text>
-            <TextInput />
+            <TextInput value={content} onChangeText={(content) => setContent(content)} />
         </View>
     );
 
