ed4af9bf4bf0cfdbe3d9774ea59f43fa4799d64d
Denis <denis.tojaga@edu.fit.ba>
2021-03-19 15:25:39 +0100

    //state adding

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Context } from "../context/BlogContext";
 
 
 
@@ -11,9 +12,29 @@ const EditScreen = ({ navigation }) => {
 
   const recievedID = navigation.getParam("id");
 
+  //we destructurize state variable from Context to get all the states
+  const { state } = useContext(Context);
+
+
+  //searching for the state with given id
+  const blogPost = state.find((blogPost) => blogPost.id == recievedID);
+
+
+
+  //if we are editing,the initial state for useState won't be an empty string
+  //it will be a current title that needs to be changed
+  const [title, setTitle] = useState(blogPost.title);
+
+
+  //we do the same thing for content
+  const [content, setContent] = useState(blogPost.content);
+
+
+
+
   return (
     <View>
-      <Text>Welcome to edit screen! - {navigation.getParam("id")}</Text>
+      <Text>Welcome to edit screen! - {recievedID}</Text>
     </View>
   );
 
