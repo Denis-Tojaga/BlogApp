47f8d9b5e37aec669b75933b95af7f1be28751cb
Denis <denis.tojaga@edu.fit.ba>
2021-03-22 18:19:30 +0100

    //blogInfo styling finished

--- src/components/BlogPostInfo.js
+++ src/components/BlogPostInfo.js
@@ -7,17 +7,66 @@ import { StyleSheet, Text, View } from "react-native";
 const BlogPostInfo = ({ blogTitle, blogContent }) => {
 
     return (
-        <View>
-            <Text>{blogTitle}</Text>
-        </View>
-    );
+        <>
+            <View style={styles.container}>
+                <Text style={styles.label}>Blog title:</Text>
+                <Text style={styles.title}>{blogTitle}</Text>
+            </View>
 
+            <View style={styles.container2}>
+                <Text style={styles.label2}>Blog content</Text>
+                <Text style={styles.content}>{blogContent}</Text>
+            </View>
+
+        </>
+    );
 };
 
 
 
 
 const styles = StyleSheet.create({
+
+    container: {
+        marginHorizontal: 22,
+        marginVertical: 25,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+
+    container2: {
+        marginHorizontal: 22,
+        marginVertical: 25,
+    },
+
+    label: {
+        fontSize: 32,
+        fontWeight: "bold",
+    },
+
+    label2: {
+        fontSize: 32,
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+
+
+    title: {
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "600",
+        paddingTop: 10,
+        marginLeft: 25
+    },
+
+    content: {
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "600",
+        textAlign: "left",
+        marginTop: 15
+    },
+
 
 });
 
