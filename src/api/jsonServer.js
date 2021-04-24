4fd08756b7f66912b6a4e7161369745c37c97a7a
Denis <denis.tojaga@edu.fit.ba>
2021-04-24 17:46:19 +0200

    //adding images

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -8,5 +8,5 @@ import axios from "axios";
 //because ngrok lasts only for that time
 
 export default axios.create({
-    baseURL: "http://6e9a5c09e8fe.ngrok.io"
+    baseURL: "http://8cee6165fb5a.ngrok.io"
 });
